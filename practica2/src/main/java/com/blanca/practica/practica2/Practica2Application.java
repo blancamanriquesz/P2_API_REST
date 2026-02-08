d75fc47d5d28cc68e5ffb1d008139e2ff1f4e16c
Blanca Manrique <157681296+blancamanriquesz@users.noreply.github.com>
2026-02-08 18:41:27 +0100

    Update Practica2Application.java

--- practica2/src/main/java/com/blanca/practica/practica2/Practica2Application.java
+++ practica2/src/main/java/com/blanca/practica/practica2/Practica2Application.java
@@ -3,7 +3,7 @@ package com.blanca.practica.practica2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.blanca.practica.practica2")
+@SpringBootApplication
 public class Practica2Application {
     public static void main(String[] args) {
         SpringApplication.run(Practica2Application.class, args);
